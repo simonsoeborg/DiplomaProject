8205f25d2bb08c788a5d9f21ec3241d2c891f45a
Simon Søhår <s173255@student.dtu.dk>
2023-02-10 11:34:14 +0100

    Adds docker

--- GroenlundAPI/obj/Debug/net6.0/GroenlundAPI.AssemblyInfo.cs
+++ GroenlundAPI/obj/Debug/net6.0/GroenlundAPI.AssemblyInfo.cs
@@ -11,6 +11,7 @@
 using System;
 using System.Reflection;
 
+[assembly: Microsoft.Extensions.Configuration.UserSecrets.UserSecretsIdAttribute("c81679f7-7cd8-4f65-81ef-3ffc91b09d83")]
 [assembly: System.Reflection.AssemblyCompanyAttribute("GroenlundAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
