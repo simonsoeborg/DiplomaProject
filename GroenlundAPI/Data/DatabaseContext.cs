72cc7bbe251f5689e2af273b7dfa06ba7ab6fc13
Simon Søhår <s173255@student.dtu.dk>
2023-02-13 11:10:40 +0100

    Adds .gitignore and DB Context

---
+++ GroenlundAPI/Data/DatabaseContext.cs
@@ -0,0 +1,18 @@
+﻿using GroenlundJWT.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace GroenlundAPI.Data
+{
+    public class DatabaseContext : DbContext
+    {
+        public DatabaseContext()
+        {
+        }
+
+        public DatabaseContext(DbContextOptions<DatabaseContext> options) : base(options)
+        {
+        }
+
+        public virtual DbSet<UserAuth>? UserAuths { get; set; }
+    }
+}
