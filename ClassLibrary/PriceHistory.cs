3c16ed6d916f1879b52764069a7c84b7353f59d5
Simon Fridolf <fridolf.lindhard@gmail.com>
2023-03-09 16:26:53 +0100

    ProductItem and priceHistory update

---
+++ ClassLibrary/PriceHistory.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary
+{
+    class PriceHistory
+    {
+        public int Id { get; set; }
+
+        public int ProductItemId { get; set; }
+
+        public DateTime ChangeDate { get; set; }
+        public decimal Price { get; set; }
+
+        public ProductItem ProductItem { get; set; }
+    }
+}
--- ClassLibrary/ProductItem.cs
+++ ClassLibrary/ProductItem.cs
@@ -4,13 +4,22 @@ namespace ClassLibrary
     public class ProductItem
     {
         public int Id { get; set; }
+
+        public int ProductId { get; set; }
         public string Condition { get; set; } = null!;
         public string Quality { get; set; } = null!;
         public sbyte Sold { get; set; }
         public decimal? Weight { get; set; }
-        public int Quantity { get; set; }
 
-        public int ProductId { get; set; }
+        public decimal PurchasePrice { get; set; }
+        public decimal CurrentPrice { get; set; }
+
+        public DateTime CreatedDate { get; set; }
+        public DateTime SoldDate { get; set; }
+
+
         public Product Product { get; set; }
+        public ICollection<PriceHistory> PriceHistories { get; set; }
+
     }
 }
