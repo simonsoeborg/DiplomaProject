0420eaf277589fc33e91ded4f9b801d45d8cab60
Kasper Baun <kasperbaun@hotmail.com>
2023-03-16 11:19:24 +0100

    Added image to product item

--- ClassLibrary/Migrations/20230316101446_AddImageToProductItem.Designer.cs
+++ ClassLibrary/Migrations/20230316101446_AddImageToProductItem.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using ClassLibrary;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,10 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace ClassLibrary.Migrations
 {
     [DbContext(typeof(GroenlundDbContext))]
-    partial class GroenlundDbContextModelSnapshot : ModelSnapshot
+    [Migration("20230316101446_AddImageToProductItem")]
+    partial class AddImageToProductItem
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
