0bb6f7d0d2854b51d426bb9ab2a2da6603f3d4af
Simon Søhår <s173255@student.dtu.dk>
2023-03-20 13:34:56 +0100

    Cors error?

--- API/Program.cs
+++ API/Program.cs
@@ -41,10 +41,18 @@ builder.Services.AddSingleton<IWebDriverService, WebDriverService>();
 builder.Services.AddTransient<SniperHandler>();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-builder.Services.AddCors();
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAll", builder =>
+    {
+        builder.AllowAnyOrigin()
+        .AllowAnyMethod()
+        .AllowAnyHeader();
+    });
+});
 var app = builder.Build();
 
-app.UseCors(builder => builder.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
+app.UseCors("AllowAll");
 app.UseSwagger();
 app.UseSwaggerUI();
 app.UseHttpsRedirection();
