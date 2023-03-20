425a6118e9fb59e89596acc6e2ce6ebf72ee9d61
Kasper Baun <kasperbaun@hotmail.com>
2023-03-20 11:24:16 +0100

    Added Material and Design to Product class in CsvConverter

--- ClassLibrary/Product.cs
+++ ClassLibrary/Product.cs
@@ -16,15 +16,31 @@ namespace ClassLibrary
         public int SubcategoryId { get; set; }
         public Subcategory Subcategory { get; set; }
 
+        public override string ToString()
+        {
+
+            return "Id: " + Id +
+                " Name: " + Name +
+                " ModelNumber: " + ModelNumber +
+                " Material: " + Material +
+                " Design: " + Design +
+                " Dimension: " + Dimension +
+                " SubcategoryId: " + SubcategoryId;
+
+        }
     }
 
     public enum MaterialType
     {
+        undefined = 0,
         porcelain = 1,
         steel = 2,
         glass = 3,
         gold = 4,
         silver = 5,
+        ceramics = 6,
+        stoneware = 7,
+        fajance = 8,
         // etc..
     }
 }
--- CsvConverter/Program.cs
+++ CsvConverter/Program.cs
@@ -1,15 +1,166 @@
 ﻿using ClassLibrary;
 using CsvConverter;
+using System.Data.SqlTypes;
 
 class Program
 {
-    public RegexHelper RegexHelper = new RegexHelper();
+    public RegexHelper RegexHelper = new();
 
     static void Main(string[] args)
     {
-        List<string[]> data = new();
+        List<string[]> data = ReadCsv("./products.csv");
 
-        using (var reader = new StreamReader("./products.csv"))
+        List<Product> Products = new();
+        //List<ProductItem> ProductItems = new();
+        List<string> names = new();
+
+        for (int i = 1; i < data.Count; i++)
+        {
+            var dataItem = data[i];
+            names.Add(dataItem[2]);
+            Products.Add(ExtractProduct(dataItem));
+            // TODO: Create ExtractProductItem(dataItem) method
+
+            // TODO: Insert all products and productItems into the database
+        }
+        //RegexHelper.TestRegexFilter(names.ToArray(), RegexHelper.RegexMap());
+        int productCounter = 0;
+        int materialCounter = 0;
+        int modelNumbercounter = 0;
+        int designCounter = 0;
+        foreach (var product in Products)
+        {
+            if (product.Material != MaterialType.undefined)
+            {
+                materialCounter++;
+            }
+            if (product.ModelNumber != "")
+            {
+                modelNumbercounter++;
+            }
+            if (product.Design != "")
+            {
+                designCounter++;
+            }
+            if (product.ModelNumber != "" && product.Material != MaterialType.undefined && product.Design != null)
+            {
+                productCounter++;
+                //Console.WriteLine(product.ToString());
+            }
+        }
+
+        Console.WriteLine("Found {0} material and {1} modelnumbers and {2} designs", materialCounter, modelNumbercounter, designCounter);
+
+        Console.WriteLine("Added data to {0}/{1} products generated from {2} dataItems", productCounter, Products.Count, data.Count);
+    }
+
+    public static Product ExtractProduct(string[] dataItem)
+    {
+        Product product = new();
+        string[] nameAndModelNumber = RegexHelper.RecognizeModelnumberPattern(dataItem[2]);
+        product.ModelNumber = nameAndModelNumber[0];
+        product.Name = nameAndModelNumber[1];
+        product.Material = ExtractMaterialType(dataItem[3]);
+        product.Design = ExtractDesign(dataItem[3]);
+
+        // TODO: Dimension
+
+        // TODO: Subcategory
+        return product;
+    }
+    public static string ExtractDesign(string input)
+    {
+        string result = "";
+        if (input.Contains("Design"))
+        {
+            int startIndex = input.IndexOf("Design");
+            string subString = input.Substring(startIndex + "Design".Length, 40);
+
+            // Trim various characters;
+            subString = TrimString(subString, "&nsbp");
+            subString = TrimString(subString, "\"");
+            subString = TrimString(subString, ";");
+            subString = TrimString(subString, ":");
+            subString = TrimString(subString, "\"");
+            subString = subString.Trim();
+
+            if (subString.Contains("<"))
+            {
+                int charIndex = subString.IndexOf("<");
+                subString = subString.Substring(0, charIndex);
+            }
+
+            result = subString;
+        }
+
+        return result;
+    }
+    public static string TrimString(string input, string removeValue)
+    {
+        string result = input;
+        if (input.Contains(removeValue))
+        {
+            int removeValueIndex = input.IndexOf(removeValue);
+            result = input.Substring(removeValueIndex + removeValue.Length);
+
+        }
+        return result;
+
+    }
+    public static MaterialType ExtractMaterialType(string input)
+    {
+        string substring = "";
+        if (input.Contains("Materiale") || input.Contains("Material"))
+        {
+            int startIndex = input.IndexOf("Materiale");
+            if (startIndex < 0)
+            {
+                startIndex = input.IndexOf("Material");
+            }
+            substring = input.Substring(startIndex);
+        }
+
+        if (substring.ToLowerInvariant().Contains("porcelæn") || substring.ToLowerInvariant().Contains("porcelain"))
+        {
+            return MaterialType.porcelain;
+        }
+
+        if (substring.ToLowerInvariant().Contains("stål") || substring.ToLowerInvariant().Contains("steel"))
+        {
+            return MaterialType.steel;
+        }
+        if (substring.ToLowerInvariant().Contains("glas") || substring.ToLowerInvariant().Contains("glass"))
+        {
+            return MaterialType.glass;
+        }
+        if (substring.ToLowerInvariant().Contains("guld") || substring.ToLowerInvariant().Contains("gold"))
+        {
+            return MaterialType.gold;
+        }
+        if (substring.ToLowerInvariant().Contains("sølv") || substring.ToLowerInvariant().Contains("silver"))
+        {
+            return MaterialType.silver;
+        }
+        if (substring.ToLowerInvariant().Contains("keramik") || substring.ToLowerInvariant().Contains("ceramics"))
+        {
+            return MaterialType.ceramics;
+        }
+        if (substring.ToLowerInvariant().Contains("stentøj") || substring.ToLowerInvariant().Contains("stoneware"))
+        {
+            return MaterialType.stoneware;
+        }
+        if (substring.ToLowerInvariant().Contains("fajance"))
+        {
+            return MaterialType.fajance;
+        }
+
+
+        return MaterialType.undefined;
+    }
+    public static List<string[]> ReadCsv(string filename)
+    {
+        List<string[]> data = new();
+        using (var reader = new StreamReader(filename))
         {
             string line;
             while ((line = reader.ReadLine()) != null)
@@ -49,65 +200,7 @@ class Program
                 data.Add(values.ToArray());
             }
         }
-
-        foreach (var dat in data)
-        {
-            if (dat.Length > 53 || dat.Length < 53)
-            {
-                Console.WriteLine("Outlier");
-                foreach (string item in dat)
-                {
-                    //Console.WriteLine(item);
-                }
-            }
-        }
-
-        List<Product> Products = new();
-        //List<ProductItem> ProductItems = new();
-        List<string> names = new();
-        List<int> lengths = new();
-
-        for (int i = 1; i < data.Count; i++)
-        {
-            var dataItem = data[i];
-            names.Add(dataItem[2]);
-            Products.Add(ExtractProduct(dataItem));
-            // TODO: Create ExtractProductItem(dataItem) method
-
-            // TODO: Insert all products and productItems into the database
-        }
-        //RegexHelper.TestRegexFilter(names.ToArray(), RegexHelper.RegexMap());
-
-        int counter = 0;
-        foreach (var product in Products)
-        {
-            if (product.ModelNumber != "")
-            {
-                counter++;
-                //Console.WriteLine("Name: {0}\nModelNumber: {1}\n", product.Name, product.ModelNumber);
-            }
-        }
-
-        Console.WriteLine("Added data to {0}/{1} products generated from {2} dataItems", counter, Products.Count, data.Count);
-    }
-
-    public static Product ExtractProduct(string[] dataItem)
-    {
-        Product product = new();
-        string[] nameAndModelNumber = RegexHelper.RecognizeModelnumberPattern(dataItem[2]);
-        product.ModelNumber = nameAndModelNumber[0];
-        product.Name = nameAndModelNumber[1];
-        //Console.WriteLine("dataItem[11]: {0}", dataItem[11]);
-        // TODO: Find a way to extract the materialType, Design, Dimension and Subcategory from the rest of the data
-
-        // TODO: MaterialType
-
-        // TODO: Design
-
-        // TODO: Dimension
-
-        // TODO: Subcategory
-        return product;
+        return data;
     }
 }
 
