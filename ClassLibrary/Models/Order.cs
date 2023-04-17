f8b1feef02e1dd8bca3545a744d5ff9d9b0ccba4
Simon Fridolf <fridolf.lindhard@gmail.com>
2023-04-17 12:40:18 +0200

    Orderchanges + check berfore deployement

--- ClassLibrary/Models/Order.cs
+++ ClassLibrary/Models/Order.cs
@@ -4,11 +4,11 @@
     {
         public int Id { get; set; }
         public int CustomerId { get; set; }
-        public int PaymentId { get; set; }
         public int ProductItemId { get; set; }
         public string PaymentStatus { get; set; } = null!;
         public string DeliveryStatus { get; set; } = null!;
         public string? DiscountCode { get; set; }
         public bool Active { get; set; }
+        public ICollection<OrderElements> OrderElements { get; } = new List<OrderElements>(); // Collection navigation containing dependents
     }
 }
---
+++ ClassLibrary/Models/OrderElements.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary.Models
+{
+    public class OrderElements
+    {
+        public int Id { get; set; } 
+        public int OrderId { get; set; }
+        public int ProductItemId { get; set; }
+
+        public Order Order { get; set; } = null!;
+        public ProductItem Item { get; set; } = null!;
+    }
+}
