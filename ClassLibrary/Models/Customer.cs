64517980ff12b62ae1b4edbf382c7c7b6b881a32
Simon Søborg <s173255@student.dtu.dk>
2023-05-04 20:21:56 +0200

    Adds more fields to Customer

--- ClassLibrary/Models/Customer.cs
+++ ClassLibrary/Models/Customer.cs
@@ -4,13 +4,18 @@ namespace ClassLibrary.Models
     {
         public int Id { get; set; }
 
-        public string FirstName { get; set; } = null!;
+        public string FirstName { get; set; }
 
-        public string LastName { get; set; } = null!;
+        public string LastName { get; set; }
 
-        public int? Phone { get; set; }
+        public int Phone { get; set; }
 
-        public string? Email { get; set; }
+        public string Email { get; set; }
+        public string Address { get; set; }
+        public string ZipCode { get; set; }
+        public string City { get; set; }
+        public string Country { get; set; }
+        public string CountryCode { get; set; }
 
         public virtual ICollection<Order> Orders { get; } = new List<Order>();
     }
