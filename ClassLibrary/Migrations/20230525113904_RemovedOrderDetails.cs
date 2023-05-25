909aa84f1eb629ecd52b876885e4e87e347d95be
Kasper Baun <kasperbaun@hotmail.com>
2023-05-25 13:40:16 +0200

    RemovedOrderDetails migration

---
+++ ClassLibrary/Migrations/20230525113904_RemovedOrderDetails.cs
@@ -0,0 +1,73 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ClassLibrary.Migrations
+{
+    /// <inheritdoc />
+    public partial class RemovedOrderDetails : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "OrderDetails");
+
+            migrationBuilder.DropColumn(
+                name: "PaymentStatus",
+                table: "Orders");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Orders_PaymentId",
+                table: "Orders",
+                column: "PaymentId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Orders_Payments_PaymentId",
+                table: "Orders",
+                column: "PaymentId",
+                principalTable: "Payments",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Orders_Payments_PaymentId",
+                table: "Orders");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Orders_PaymentId",
+                table: "Orders");
+
+            migrationBuilder.AddColumn<string>(
+                name: "PaymentStatus",
+                table: "Orders",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.CreateTable(
+                name: "OrderDetails",
+                columns: table => new
+                {
+                    Active = table.Column<bool>(type: "bit", nullable: false),
+                    CustomerId = table.Column<int>(type: "int", nullable: false),
+                    DeliveryStatus = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    DiscountCode = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Manufacturer = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    OrderId = table.Column<int>(type: "int", nullable: false),
+                    PaymentId = table.Column<int>(type: "int", nullable: false),
+                    PaymentStatus = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    ProductId = table.Column<int>(type: "int", nullable: false),
+                    ProductItemId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                });
+        }
+    }
+}
