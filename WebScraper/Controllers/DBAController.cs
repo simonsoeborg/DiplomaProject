25cbd648da81d59c787fff94e515e318c95c34b9
Simon Søborg <s173255@student.dtu.dk>
2023-06-02 10:54:05 +0200

    FIxes Dba Search Problem

--- WebScraper/Controllers/DBAController.cs
+++ WebScraper/Controllers/DBAController.cs
@@ -33,73 +33,89 @@ namespace WebScraper.Controllers
                 wait.Until(ExpectedConditions.ElementToBeClickable(By.CssSelector("button[class='btn btn-large btn-search'"))).Click();
                 //html/body/div[4]/header/div/div/form/div/button
 
-                wait.Until(ExpectedConditions.ElementExists(By.CssSelector("tr.dbaListing.listing")));
-                var tableEntries = _driver.FindElements(By.CssSelector("tr.dbaListing.listing"));
-
-                int chunkSize = 10;
-                var chunks = tableEntries.Select((value, index) => new { Index = index, Value = value })
-                                          .GroupBy(x => x.Index / chunkSize)
-                                          .Select(x => x.Select(v => v.Value).ToList())
-                                          .ToList();
-
-                List<Thread> threads = new List<Thread>();
-                foreach (var chunk in chunks)
+                try
                 {
-                    var thread = new Thread(() =>
+                    wait.Until(ExpectedConditions.ElementExists(By.CssSelector("tr.dbaListing.listing")));
+                } catch (Exception ex)
+                {
+                    Console.WriteLine("Didnt find anything on DBA");
+                }
+                if (_driver.FindElements(By.CssSelector("tr.dbaListing.listing")) != null)
+                {
+                    var tableEntries = _driver.FindElements(By.CssSelector("tr.dbaListing.listing"));
+
+                    int chunkSize = 10;
+                    var chunks = tableEntries.Select((value, index) => new { Index = index, Value = value })
+                                              .GroupBy(x => x.Index / chunkSize)
+                                              .Select(x => x.Select(v => v.Value).ToList())
+                                              .ToList();
+
+                    List<Thread> threads = new List<Thread>();
+                    foreach (var chunk in chunks)
                     {
-                        foreach (IWebElement tableEntry in chunk)
+                        var thread = new Thread(() =>
                         {
-                            IWebElement descriptionElement = null;
-                            IWebElement script = null;
-                            string description = "";
-                            string scriptData = "";
-                            JObject json = null;
-                            var model = new ScrapingModel();
-
-                            int i = tableEntries.IndexOf(tableEntry) + 1;
-
-                            if (!(i >= 11))
+                            foreach (IWebElement tableEntry in chunk)
                             {
-                                wait.Until(d => d.FindElements(By.XPath("//script[@type='application/ld+json']")).Count > 0);
-                                script = tableEntry.FindElement(By.XPath(".//script[@type='application/ld+json']"));
-                                scriptData = script.GetAttribute("innerHTML"); 
-                                descriptionElement = tableEntry.FindElement(By.XPath(".//span[@class='text']"));
+                                IWebElement descriptionElement = null;
+                                IWebElement script = null;
+                                string description = "";
+                                string scriptData = "";
+                                JObject json = null;
+                                var model = new ScrapingModel();
 
-                                description = descriptionElement.Text;
+                                int i = tableEntries.IndexOf(tableEntry) + 1;
 
-                                json = JObject.Parse(scriptData);
-                                string? imageUrl = json["image"]?.ToString();
-                                string? name = json["name"]?.ToString();
-                                string? itemUrl = json["url"]?.ToString();
-                                string? price = json["offers"]["price"]?.ToString();
+                                if (!(i >= 11))
+                                {
+                                    wait.Until(d => d.FindElements(By.XPath("//script[@type='application/ld+json']")).Count > 0);
+                                    script = tableEntry.FindElement(By.XPath(".//script[@type='application/ld+json']"));
+                                    scriptData = script.GetAttribute("innerHTML");
+                                    descriptionElement = tableEntry.FindElement(By.XPath(".//span[@class='text']"));
 
-                                Console.WriteLine($"Image URL: {imageUrl}");
-                                Console.WriteLine($"Item URL: {itemUrl}");
-                                Console.WriteLine($"Price: {price}");
-                                Console.WriteLine($"Description: {description}");
+                                    description = descriptionElement.Text;
+
+                                    json = JObject.Parse(scriptData);
+                                    string? imageUrl = json["image"]?.ToString();
+                                    string? name = json["name"]?.ToString();
+                                    string? itemUrl = json["url"]?.ToString();
+                                    string? price = json["offers"]["price"]?.ToString();
+
+                                    Console.WriteLine($"Image URL: {imageUrl}");
+                                    Console.WriteLine($"Item URL: {itemUrl}");
+                                    Console.WriteLine($"Price: {price}");
+                                    Console.WriteLine($"Description: {description}");
 
 #pragma warning disable CS8601 // Possible null reference assignment.
-                                model.DBAItemImages = imageUrl;
-                                model.DBAItemLink = itemUrl;
-                                model.DBAItemPrice = price;
-                                model.DBAItemDescription = description;
-                                model.Source = "dba.dk";
-                                model.ItemTitle = name.Replace("&amp;quot;", "").Replace("...", "");
+                                    model.DBAItemImages = imageUrl;
+                                    model.DBAItemLink = itemUrl;
+                                    model.DBAItemPrice = price;
+                                    model.DBAItemDescription = description;
+                                    model.Source = "dba.dk";
+                                    model.ItemTitle = name.Replace("&amp;quot;", "").Replace("...", "");
 #pragma warning restore CS8601 // Possible null reference assignment.
-                                results.Add(model);
+                                    results.Add(model);
+                                }
                             }
-                        }
-                    });
-                    threads.Add(thread);
-                    thread.Start();
-                }
+                        });
+                        threads.Add(thread);
+                        thread.Start();
+                    }
 
-                foreach (Thread thread in threads)
-                {
-                    thread.Join();
+                    foreach (Thread thread in threads)
+                    {
+                        thread.Join();
+                    }
                 }
             }
-            return results.ToList();
+            
+            if(results.Count > 0)
+            {
+                return results.ToList();
+            } else
+            {
+                return new List<ScrapingModel>();
+            }
         }
     }
 }
